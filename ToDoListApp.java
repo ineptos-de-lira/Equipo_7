5ea542c47349381a8d27f7e9e9e46d7d70560421
NatalyGuzmánRomo <up240436@alumnos.upa.edu.mx>
2025-12-02 14:47:20 -0600

    Agregar Menú

---
+++ ToDoListApp.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class ToDoListApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> tareas = new ArrayList<>();
+
+        int opcion = -1;
+
+        do {
+            System.out.println("\n=========================");
+            System.out.println("        TO-DO LIST         ");
+            System.out.println("===========================");
+            System.out.println("1. Agregar tarea");
+            System.out.println("2. Modificar tarea");
+            System.out.println("3. Eliminar tarea ");
+            System.out.println("4. Mostrar tareas");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opción: ");
+
+           
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    System.out.println("Saliendo del sistema. ¡Adiós!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+            }
+
+            
+
+        } while (opcion != 0);
+
+        scanner.close();
+    }
+}
